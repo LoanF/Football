b674d14cdec0a78b2031c5941b1561b87bd0a91e
LoanF <96796739+LoanF@users.noreply.github.com>
2023-05-21 14:20:44 +0200

    Add files via upload

--- class/GestionArticle.php
+++ class/GestionArticle.php
@@ -50,4 +50,29 @@ class GestionArticle {
             echo "".$e->getMessage();
         }
     }
+    
+    public function addArticle(string $titre, string $desc, string $auteur, string $picture) {
+        $req="INSERT INTO article(titre_article, desc_article, auteur_article, image_article) VALUES(:titre, :desc, :auteur, :image);";
+        
+        $res = $this->cnx->prepare($req);
+        
+        $res->bindParam(':titre', $titre);
+        $res->bindParam(':desc', $desc);
+        $res->bindParam(':auteur', $auteur);
+        $res->bindParam(':image', $picture);
+        
+        try{
+            $res->execute();
+        }catch(PDOException $e){
+            echo "".$e->getMessage();
+        }
+    }
+    
+    public function removeArt(int $id) {
+        $req="DELETE FROM commentaire WHERE id_article=".$id;
+        $this->cnx->exec($req);
+        
+        $req="DELETE FROM article WHERE id_article=".$id;
+        $this->cnx->exec($req);
+    }
 }
